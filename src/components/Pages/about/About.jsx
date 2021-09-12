90fa45d625761d3606dbf700b78692f2d6359e38
Rahmonjon Ibragimov <rahmonjonibragimov@Rahmonjons-Mac-mini.local>
2021-09-12 10:36:01 +0200

    update

--- src/components/Pages/about/About.jsx
+++ src/components/Pages/about/About.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
-import './about.scss';
-
+import "./about.scss";
 export default function About() {
     return (
         <div className="about" id="about">
-            <h1>about</h1>
+            <div class="container">
+                <h2 >About Me</h2>
+                <p>I'm Junior Web Developer.
+                    My Passion is Front-end & Back-end in this field
+                </p>
+                <p>
+                    Passionate about personal growth. It was just decade ago when I discovered my passion for growing and helping others grow. At that time, I was 19 and in my final year of College when I learnt that "Whenever we think we are good, we can be even better".
+
+                    I have been living in Scandinavia and Baltic Countries for the last decade before I moved to Germany in the middle of 2019, Since then I have been learning German (currently B2) language while working in German industry.
+
+                    I had the opportunity to become as Fullstack: Web-developer in 2020. since then I have been studying and doing daily exercises in Digital Career Institute here in Düsseldorf.
+
+                    My specialties include quickly learning new skills and programming languages, problem solving, responsive design principles, website optimisation.
+
+                    So far I have JavaScript, HTML, CSS, jQuery, Bootstrap, and Git/GitHub under my belt.
+                    Currently I'm improving in learning React, SQL, Node.JS, MongoDB, and Express.JS. I'm still enthusiastically grabbing onto any other programming languages, frameworks, or principles I can integrate into the coding web in my head.
+                </p>
+            </div>
         </div>
     )
 }
--- src/components/Pages/home/Home.jsx
+++ src/components/Pages/home/Home.jsx
@@ -1,9 +1,39 @@
-import React from 'react';
-import './home.scss';
+
+import "./home.scss";
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
+
 export default function Home() {
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed:60,
+      strings: ["Developer", "Designer", "Content Creator"],
+    });
+  }, []);
+
   return (
     <div className="home" id="home">
-    <h1>home</h1>
-</div>
-  )
+      <div className="left">
+        <div className="imgContainer">
+          <img src="picture/man.png" alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Rahmonjon Ibragimov</h1>
+          <h3>
+          Full Stack  <span ref={textRef}></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="picture/down.png" alt="" />
+        </a>
+      </div>
+    </div>
+  );
 }
