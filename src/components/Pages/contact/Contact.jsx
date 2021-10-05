dc8e2a2134ec6d5fb5ab4c1ecc6db6ab82bc0a80
Rahmonjon22 <rahmonjon2@yahoo.com>
2021-10-05 11:14:23 +0200

    update

--- src/components/Pages/contact/Contact.jsx
+++ src/components/Pages/contact/Contact.jsx
@@ -1,14 +1,23 @@
-import { useState } from "react";
-
+import React, { useRef } from 'react';
+import emailjs from "emailjs-com";
 import "./contact.scss";
+import { init } from 'emailjs-com';
+init("user_0Y21E2NeaaDZCFgWUdJU6");
+
 export default function Contact() {
-  const [message, setMessage] = useState(false);
+  const form = useRef();
 
-  const handleSubmit = (e) => {
+  const sendEmail = (e) => {
     e.preventDefault();
-    setMessage(true);
-  };
 
+    emailjs.sendForm('service_eifvqbi', 'template_s19yzek', form.current, 'user_0Y21E2NeaaDZCFgWUdJU6')
+      .then((result) => {
+          console.log("Correct, ", result.text);
+      }, (error) => {
+          console.log("Error part ", error.text);
+      });
+      e.target.reset()
+  };
   return (
 
     <div className="contact" id="contact">
@@ -18,14 +27,13 @@ export default function Contact() {
         <iframe className="map" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d4987.854363779201!2d7.379167000000001!3d51.312464!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47b92510cd1e39ef%3A0xca0dbe1c99f788ec!2sR%C3%B6thelteich%2038%2C%2058256%20Ennepetal%2C%20Germany!5e0!3m2!1sen!2sus!4v1633285724472!5m2!1sen!2sus"
           allowfullscreen="" loading="lazy">
         </iframe>
-        <form onSubmit={handleSubmit} action="https://formsubmit.co/rahmonjon2@yahoo.com" method="POST" >
+        <form ref={form}  onSubmit={sendEmail}  method="POST" >
           <h3>Fill form</h3>
-          <input type="text" placeholder="Your name" className="box" />
-          <input type="email" placeholder="Your email" className="box" />
-          <input type="tel" placeholder="number" className="box" />
-          <textarea placeholder="your message" class="box" cols="30" rows="10"></textarea>
-          <input type="submit" value="send message" className="btn" />
-          {message} && <span>Thank you for your message, I will soon contact you :</span>;
+          <input type="text" placeholder="name" name="from_name" required className="box" />
+          <input type="email" placeholder="email" name="email" required className="box" />
+          <input type="tel" placeholder="number" name="number" className="box" />
+          <textarea placeholder="your message" name="message" class="box" cols="30" rows="10"></textarea>
+          <button type="submit" className="btn">Send</button>
         </form>
       </div>
     </div>
