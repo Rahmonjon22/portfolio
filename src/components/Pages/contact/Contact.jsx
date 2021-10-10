c9687c317ba63e3301fb6549f08f2bc12566c1dd
Rahmonjon22 <rahmonjon2@yahoo.com>
2021-10-10 23:07:49 +0200

    update

--- src/components/Pages/contact/Contact.jsx
+++ src/components/Pages/contact/Contact.jsx
@@ -12,25 +12,27 @@ export default function Contact() {
 
     emailjs.sendForm('service_eifvqbi', 'template_s19yzek', form.current, 'user_0Y21E2NeaaDZCFgWUdJU6')
       .then((result) => {
-          console.log("Correct, ", result.text);
+        console.log("Correct, ", result.text);
       }, (error) => {
-          console.log("Error part ", error.text);
+        console.log("Error part ", error.text);
       });
-      e.target.reset()
+    e.target.reset()
   };
   return (
 
     <div className="contact" id="contact">
 
       <h1 className="heading"><span className="span1">Contact</span>Me</h1>
-      <div className="row">       
-        <form ref={form}  onSubmit={sendEmail}  method="POST" >
-          <h3>Fill form</h3>
+      <div className="row">
+        <form ref={form} onSubmit={sendEmail} method="POST" >
+          <h3>Fill the form</h3>
           <input type="text" placeholder="name" name="from_name" required className="box" />
           <input type="email" placeholder="email" name="email" required className="box" />
           <input type="tel" placeholder="number" name="number" className="box" />
           <textarea placeholder="your message" name="message" class="box" cols="30" rows="10"></textarea>
-          <button type="submit" className="btn">Send</button>
+          <div class="wrapper">
+            <button type="submit"><span>Send</span></button>
+          </div>
         </form>
         <iframe className="map" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d4987.854363779201!2d7.379167000000001!3d51.312464!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47b92510cd1e39ef%3A0xca0dbe1c99f788ec!2sR%C3%B6thelteich%2038%2C%2058256%20Ennepetal%2C%20Germany!5e0!3m2!1sen!2sus!4v1633285724472!5m2!1sen!2sus"
           allowfullscreen="" loading="lazy">
@@ -39,3 +41,4 @@ export default function Contact() {
     </div>
   )
 }
+
