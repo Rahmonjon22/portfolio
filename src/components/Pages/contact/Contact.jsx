07b1a14581bfc285ac40140dd28e3f4a21e5dc84
Rahmonjon22 <rahmonjon2@yahoo.com>
2021-10-10 22:22:39 +0200

    update

--- src/components/Pages/contact/Contact.jsx
+++ src/components/Pages/contact/Contact.jsx
@@ -23,10 +23,7 @@ export default function Contact() {
     <div className="contact" id="contact">
 
       <h1 className="heading"><span className="span1">Contact</span>Me</h1>
-      <div className="row">
-        <iframe className="map" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d4987.854363779201!2d7.379167000000001!3d51.312464!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47b92510cd1e39ef%3A0xca0dbe1c99f788ec!2sR%C3%B6thelteich%2038%2C%2058256%20Ennepetal%2C%20Germany!5e0!3m2!1sen!2sus!4v1633285724472!5m2!1sen!2sus"
-          allowfullscreen="" loading="lazy">
-        </iframe>
+      <div className="row">       
         <form ref={form}  onSubmit={sendEmail}  method="POST" >
           <h3>Fill form</h3>
           <input type="text" placeholder="name" name="from_name" required className="box" />
@@ -35,6 +32,9 @@ export default function Contact() {
           <textarea placeholder="your message" name="message" class="box" cols="30" rows="10"></textarea>
           <button type="submit" className="btn">Send</button>
         </form>
+        <iframe className="map" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d4987.854363779201!2d7.379167000000001!3d51.312464!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47b92510cd1e39ef%3A0xca0dbe1c99f788ec!2sR%C3%B6thelteich%2038%2C%2058256%20Ennepetal%2C%20Germany!5e0!3m2!1sen!2sus!4v1633285724472!5m2!1sen!2sus"
+          allowfullscreen="" loading="lazy">
+        </iframe>
       </div>
     </div>
   )
