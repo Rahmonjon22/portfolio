e05cd0188b9da37ef9ea7894f2f08820edadbb48
Rahmonjon22 <rahmonjon2@yahoo.com>
2022-06-15 09:16:00 +0200

    Just removing unnecessary things and adding some changes

--- src/components/Pages/contact/Contact.jsx
+++ src/components/Pages/contact/Contact.jsx
@@ -22,10 +22,9 @@ export default function Contact() {
 
     <div className="contact" id="contact">
 
-      <h1 className="heading"><span className="span1">Contact</span>Me</h1>
+      <h1 className="heading"><span className="span1">Contact </span></h1>
       <div className="row">
         <form ref={form} onSubmit={sendEmail} method="POST" >
-          <h3>Fill the form</h3>
           <input type="text" placeholder="name" name="from_name" required className="box" />
           <input type="email" placeholder="email" name="email" required className="box" />
           <input type="tel" placeholder="number" name="number" className="box" />
@@ -34,9 +33,6 @@ export default function Contact() {
             <button type="submit"><span>Send</span></button>
           </div>
         </form>
-        <iframe className="map" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d4987.854363779201!2d7.379167000000001!3d51.312464!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47b92510cd1e39ef%3A0xca0dbe1c99f788ec!2sR%C3%B6thelteich%2038%2C%2058256%20Ennepetal%2C%20Germany!5e0!3m2!1sen!2sus!4v1633285724472!5m2!1sen!2sus"
-          allowfullscreen="" loading="lazy">
-        </iframe>
       </div>
     </div>
   )
